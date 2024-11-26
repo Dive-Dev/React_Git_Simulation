9075e5e3c27a7938fb1ddd19cc4c6aafcb912d9d
Divesh S <diveshswork@gmail.com>
2024-11-26 15:43:44 +0530

    Our First Conversation in React .

---
+++ react-git-simulation/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Conversation Between Two Develpers</h1>
+      <div className="card">
+            <p>Divesh: Hey I am Divesh ? It's nice to meet you!</p>
+        </div>
+    </>
+  )
+}
+
+export default App
