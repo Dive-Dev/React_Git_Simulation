e0c24115200b81a77ccf02653f66aa59620eb252
chethan20-byte <chethansmca2025@gmail.com>
2024-11-26 15:58:12 +0530

    replying back with react

--- react-git-simulation/src/App.jsx
+++ react-git-simulation/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <h1>Conversation Between Two Develpers</h1>
       <div className="card">
             <p>Divesh: Hey I am Divesh ? It's nice to meet you!</p>
+            <p>chethan: Hi this is chethan nice to meet you . i hope you are doing well </p>
         </div>
     </>
   )
